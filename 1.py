b81da0b4c1a617a5ed73eb7b3de0efb8f0ab444b
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-10 23:50:50 +0530

    first class

---
+++ 1.py
@@ -0,0 +1,5 @@
+fname = "Saffan"
+lname = "Khan"
+
+print('My name is ' + fname + " " + lname)
+print(f"My name is {fname} {lname}")
---
+++ 2.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+addition = num1 + num2
+
+print(addition)
