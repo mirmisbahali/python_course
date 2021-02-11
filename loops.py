7cc7eec60045f5401358fda3613cbf2a951a6160
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-11 23:25:08 +0530

    for loop

--- loops.py
+++ loops.py
@@ -2,8 +2,12 @@
 while loop
 for loop
 '''
-
+'''
 count = 0 # Initialization
 while (count < 10):
     print(f"{count}")
     count = count + 1 # Increment
+'''
+
+for count in range(10):
+    print(f"{count}. Hello")
