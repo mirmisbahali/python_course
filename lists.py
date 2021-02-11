2004c860b4021500f119505ee55689c6f8a4824f
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-11 23:50:51 +0530

    pop method

--- lists.py
+++ lists.py
@@ -5,6 +5,7 @@ student3 = "Wajid"
 student4 = "Adnan"
 '''
 
+'''
 student = ['Saffan', 1, 2, 2.3, 'Misbah', 'Wajid', 'Adnan', 'Owais', 'Saqlain', 'Khaleel', 'Abdur Rahman']
 
 # print(student[0])
@@ -12,3 +13,14 @@ student = ['Saffan', 1, 2, 2.3, 'Misbah', 'Wajid', 'Adnan', 'Owais', 'Saqlain', 
 
 for i in range(len(student)):
     print(student[i])
+
+'''
+
+students = ['Saffan', 'Misbah', 'Wajid', 'Adnan']
+print("Before deleteing", students)
+students.pop(2)
+print("After deleting", students)
+
+students.append('Owais')
+print("After adding", students)
+
